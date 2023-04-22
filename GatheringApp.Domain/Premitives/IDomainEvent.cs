a1c65389dc7f1defc33775bcc5721dfd008e7d68
Mubark Khaled 22 <mubarkkhaled22@gmail.com>
2023-04-22 17:41:55 +0300

    inherate IDomainEvent by INotification Interface that comes from Mediator Library

--- GatheringApp.Domain/Premitives/IDomainEvent.cs
+++ GatheringApp.Domain/Premitives/IDomainEvent.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace GatheringApp.Domain.Premitives
+namespace GatheringApp.Domain.Premitives;
+
+
+public  interface IDomainEvent:INotification
 {
-    internal interface IDomainEvent
-    {
-    }
 }
