b3da99667461f86e90d70c3493723b603a5ae04b
Mubark Khaled 22 <mubarkkhaled22@gmail.com>
2023-04-22 17:23:43 +0300

    update Aggregate Root abstract class addding list of IDominEvent and add RisedDomainEvent method that has IDomainEvent Parameter and return void

--- GatheringApp.Domain/Premitives/AggregateRoot.cs
+++ GatheringApp.Domain/Premitives/AggregateRoot.cs
@@ -4,7 +4,16 @@ namespace GatheringApp.Domain.Premitives;
 
 public abstract class AggregateRoot : Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
     public AggregateRoot(Guid id) : base(id)
     {
     }
+
+    protected void RisedDomainEvent(IDomainEvent domainEveent)
+    {
+
+        _domainEvents.Add(domainEveent);
+    }
+
+
 }
