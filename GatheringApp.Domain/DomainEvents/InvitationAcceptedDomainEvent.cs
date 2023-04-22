2a6cc4e14cd918e9451af539435c76aeae4e297b
Mubark Khaled 22 <mubarkkhaled22@gmail.com>
2023-04-22 17:16:23 +0300

    add InvitaionAcceptedDomain Event Record to Domain Event Floder in Domain Project

---
+++ GatheringApp.Domain/DomainEvents/InvitationAcceptedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using GatheringApp.Domain.Premitives;
+
+namespace GatheringApp.Domain.DomainEvents;
+
+public sealed  record InvitationAcceptedDomainEvent
+    (
+    Guid InvitationId,  Guid GatheringId
+    ):IDomainEvent
+ 
